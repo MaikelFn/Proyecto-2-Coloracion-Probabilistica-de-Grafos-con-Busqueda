0adb9dd3074c418e563cbcf054cb2eb5f4eeaa5c
Maikel Flores Navarro <159388957+MaikelFn@users.noreply.github.com>
2025-11-08 16:57:43 -1200

    Unifique las clases en un solo archivo
    -Unifique las clases en un solo archivo para mantener el proyecto mas compacto y ordenado

--- Codigo/clases.py
+++ Codigo/clases.py
@@ -1,13 +1,43 @@
 import random
+
+class Nodo:
+    def __init__(self, ID):
+        self.ID = ID
+        self.color = None
+        self.vecinos = []
+        self.numvecinos = 0
+    
+    def colorear(self, nuevo_color):
+        self.color = nuevo_color
+    
+    def añadir_vecino(self, vecino):
+        if vecino not in self.vecinos:
+            self.vecinos.append(vecino)
+            self.numvecinos = len(self.vecinos)
+    
+    def eliminar_vecino(self, vecino):
+        if vecino in self.vecinos:
+            self.vecinos.remove(vecino)
+            self.numvecinos = len(self.vecinos)
+
+    def contar_conflictos(self):
+        contador=0
+        for vecino in self.vecinos:
+            if vecino.color == self.color:
+                contador+=1
+        return contador
+
+    def obtener_colores_validos(self, colores):
+        for vecino in self.vecinos:
+            if vecino.color in colores:
+                colores.remove(vecino.color)
+        return colores
 class Grafo:
     def __init__(self):
         self.nodos = []
         self.aristas = []
-        self.colores = []
+        self.colores = ["Azul", "Amarillo", "Morado"]
         
-    def agregar_color(self, color):
-        if color not in self.colores:
-            self.colores.append(color)
 
     def agregar_nodo(self, nodo):
         if nodo not in self.nodos:
