882d7162b9f4c3095d4c1c7ccb4a43a71e077146
Maikel Flores Navarro <159388957+MaikelFn@users.noreply.github.com>
2025-11-12 15:42:51 -1200

    Termine de migrar las 3 clases a js

--- Codigo/src/Clases.js
+++ Codigo/src/Clases.js
@@ -1,7 +1,10 @@
 export class Nodo {
-    color = null;
-    vecinos = [];
-    numvecinos = 0;
+    constructor(id) {
+    this.id = id;
+    this.color = null;
+    this.vecinos = [];
+    this.numvecinos = 0;
+    }
 
     colorear(nuevo_color) {
         this.color = nuevo_color;
@@ -10,15 +13,15 @@ export class Nodo {
     añadir_vecino(vecino) {
         if (!this.vecinos.includes(vecino)) {
             this.vecinos.push(vecino);
-            this.numvecinos = this.vecinos.length;
+            this.numvecinos++;
         }
     }
 
     eliminar_vecino(vecino) {
-        const idx = this.vecinos.indexOf(vecino);
-        if (idx !== -1) {
-            this.vecinos.splice(idx, 1);
-            this.numvecinos = this.vecinos.length;
+        const indice = this.vecinos.indexOf(vecino);
+        if (indice !== -1) {
+            this.vecinos.splice(indice, 1);
+            this.numvecinos--;
         }
     }
 
@@ -29,26 +32,95 @@ export class Nodo {
         }
         return contador;
     }
-
-    obtener_colores_validos(colores) {
-        for (const vecino of this.vecinos) {
-            const c = vecino.color;
-            const idx = colores.indexOf(c);
-            if (c != null && idx !== -1) colores.splice(idx, 1);
-        }
-        return colores;
-    }
-    constructor(valor) {
-        this.valor = valor;
-        this.siguiente = null;
-    }
 }
 export class Arista {
     constructor(nodo1, nodo2) {
-        this.origen = nodo1;
-        this.destino = nodo2;
-        this.confliocto = false;
+        this.nodo1 = nodo1;
+        this.nodo2 = nodo2;
+        this.conflicto = false;
     }
 }
 export class Grafo {
-    constructor() {}}
+    constructor() {
+        this.nodos = [];
+        this.aristas = [];
+        this.colores = ["Azul", "Amarillo", "Morado"];
+    }
+    añadir_nodo(nodo) {
+        this.nodos.push(nodo);
+    }
+    añadir_arista(nodo1, nodo2) {
+        const arista = new Arista(nodo1, nodo2);
+        this.aristas.push(arista);
+        nodo1.añadir_vecino(nodo2);
+        nodo2.añadir_vecino(nodo1);
+    }
+    eliminar_arista(arista) {
+        const indice = this.aristas.indexOf(arista);
+        if (indice !== -1) {
+            this.aristas.splice(indice, 1);
+            arista.nodo1.eliminar_vecino(arista.nodo2);
+            arista.nodo2.eliminar_vecino(arista.nodo1);
+        }
+    }
+    obtener_conexiones() {
+        const conexiones = [];
+        for (const arista of this.aristas) {
+            conexiones.push([arista.nodo1.id, arista.nodo2.id]);
+        }
+        return conexiones;
+    }
+    obtener_nodos() {
+        const ids = [];
+        for (const nodo of this.nodos) {
+            ids.push(nodo.id);
+        }
+        return ids;
+    }
+    colorear_grafo() {
+        for (const nodo of this.nodos) {
+            indice_Aleatorio = Math.floor(Math.random() * this.colores.length);
+            const color_aleatorio = this.colores[indice_Aleatorio];
+            nodo.colorear(color_aleatorio);
+        }
+        this.validar_aristas();
+    }
+    validar_aristas() {
+        for (const arista of this.aristas) {
+            if (arista.nodo1.color === arista.nodo2.color) {
+                arista.conflicto = true;
+            } else {
+                arista.conflicto = false;
+            }
+        }
+    }
+    total_conflictos() {
+        let total = 0;
+        for (const arista of this.aristas) {
+            if (arista.conflicto) total++;
+        }
+        return total;
+    }
+    validar_coloracion() {
+        if (this.total_conflictos() === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    recolorear_nodos(diccionario_colores) {
+        for (const nodo of this.nodos) {
+            if (nodo.id in diccionario_colores) {
+                nodo.colorear(diccionario_colores[nodo.id]);
+            }
+        }
+        this.validar_aristas();
+    }
+    obtener_colores() {
+        const diccionario_colores = {};
+        for (const nodo of this.nodos) {
+            diccionario_colores[nodo.id] = nodo.color;
+        }
+        return diccionario_colores;
+    }
+}
